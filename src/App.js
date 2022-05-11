d7ffbb80b04554609ef27af2d18e8b59d42b688b
Maurine Cornillon <maurinecornillon@mbp-de-maurine.lan>
2022-05-11 16:06:44 +0200

    make it better

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 import "./App.css";
 
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useState } from "react";
+
+// components
+// import Header from "./components/Header";
+
+//Import des icones
+// import { library } from "@fortawesome/fontawesome-svg-core";
+
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <Router>
+      {/* <Header /> */}
+
+      <Routes>
+        {/* <Route path="/" element={<Home />} />
+        <Route path="/" element={<Offer />} />
+        <Route path="/" element={<SignUp handleToken={handleToken} />} /> */}
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,63 @@
+import logo from "../assets/img/Logo.png";
+
+import { useNavigate } from "react-router-dom";
+
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  const navigate = useNavigate();
+
+  return (
+    <>
+      <header>
+        <div className="container">
+          <section className="header">
+            <section className="left-header">
+              <img src={logo} onClick={() => navigate("/")} alt="logo" />
+
+              <div className="Recherche">
+                <FontAwesomeIcon className="icone" icon="magnifying-glass" />
+                <input type="text" placeholder="Rechercher..." />
+              </div>
+            </section>
+            <section className="right-header">
+              <div className="contact">
+                <i className="fa-solid fa-comments"></i>
+                {!userToken ? (
+                  <>
+                    <Link to="/signup">
+                      <button>S'inscrire</button>
+                    </Link>
+                    <br />
+                    <Link to="/login">
+                      <button>Connexion</button>
+                    </Link>
+                  </>
+                ) : (
+                  <button
+                    onClick={() => {
+                      handleToken();
+                    }}
+                  >
+                    Déconnexion
+                  </button>
+                )}
+
+                <br />
+              </div>
+              <button
+                className="send-button"
+                onClick={() => navigate("/publish")}
+              >
+                Vendre tes articles
+              </button>
+            </section>
+          </section>
+        </div>
+
+        {/* <div className="space-bottom"></div> */}
+      </header>
+    </>
+  );
+};
+export default Header;
